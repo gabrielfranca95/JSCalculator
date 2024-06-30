32719b4c85c3723cf39c36572b1c48c01f3b3a7d
Gabriel Franca <gabrielfurtunatofranca@gmail.com>
2024-06-30 15:26:30 -0300

    Add complete calculator logic

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ function appendCharacter(char) {
 function clearDisplay() {
   display.textContent = "";
 }
+
+function calculateResult() {
+  try {
+    let expression = display.textContent
+      .replace("÷", "/")
+      .replace("x", "*")
+      .replace(",", ".")
+      .replace("mod", "%")
+      .replace("√", "Math.sqrt")
+      .replace("π", Math.PI);
+
+    if (expression.includes("^2")) {
+      expression = expression.replace(/\b(\d+)\^2\b/g, "Math.pow($1, 2)");
+    }
+
+    let result = eval(expression);
+
+    display.textContent = result;
+  } catch (error) {
+    display.textContent = "Erro";
+  }
+}
